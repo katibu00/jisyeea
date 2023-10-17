d996206ecb4cccf738035ee0671d5ae4565f82b3
katibu01 <ukmisau@gmail.com>
2023-10-17 15:12:01 +0100

    update

--- resources/views/pdf/acknowledgment.blade.php
+++ resources/views/pdf/acknowledgment.blade.php
@@ -1,76 +1,104 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Application Acknowledgment</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>ID Card</title>
     <style>
         body {
-            font-family: Arial, sans-serif;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            /* background-color: #E6E6E6; */
+        }
+
+        .id-card {
+            width: 300px;
+            background-color: #fff;
+            border: 2px solid #007bff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            overflow: hidden;
         }
 
         .header {
+            background-color: #007bff;
+            color: #fff;
             text-align: center;
-            margin-bottom: 30px;
+            padding: 20px;
         }
 
         .header img {
-            max-width: 150px;
+            max-width: 80px;
+            vertical-align: middle;
         }
 
-        .applicant-info {
-            border: 1px solid #000;
-            padding: 10px;
-            margin-bottom: 20px;
+        .organization-name {
+            font-size: 18px;
+            margin: 10px 0;
         }
 
-        .applicant-info p {
+        .website-url {
+            color: #fff;
+            text-decoration: none;
+            font-size: 12px;
+            display: block;
+            margin-top: 5px;
+        }
+
+        .user-details {
+            text-align: center;
+            padding: 20px;
+        }
+
+        .user-details img {
+            max-width: 120px;
+            border-radius: 50%;
+            margin: 10px 0;
+        }
+
+        .user-name {
+            font-size: 20px;
+            font-weight: bold;
             margin: 5px 0;
         }
 
-        .applicant-photo {
-            text-align: center;
-            margin-bottom: 20px;
+        .user-details p {
+            font-size: 14px;
+            margin: 5px 0;
         }
 
-        .applicant-photo img {
-            max-width: 150px;
+        .qr-code {
+            text-align: center;
+            padding: 20px;
         }
 
-        .footer {
-            text-align: center;
+        .qr-code img {
+            max-width: 100px;
         }
     </style>
 </head>
 <body>
-    <div class="header">
-        <img src="{{ public_path('/images/logo.jpeg') }}" alt="Agency Logo" width="100">
-        <h2>Jigawa State Youth Empowerment Agency</h2>
-        <p>Block A, New Secretariat Complex,3 Arms Zone, Takur Dutse, Jigawa State-Nigeria.</p>
-        <p>Phone: +234 123 456 789</p>
-    </div>
-
-    <div class="applicant-info">
-        <h3>Pre-Registration Acknowledgment</h3>
-        <p><strong>Application Number:</strong> {{ $preRegistration->application_number }}</p>
-        <p><strong>Name:</strong> {{ $preRegistration->full_name }}</p>
-        <p><strong>Date of Birth:</strong> {{ $preRegistration->date_of_birth }}</p>
-        <p><strong>Gender:</strong> {{ $preRegistration->gender }}</p>
-        <p><strong>Contact Number:</strong> {{ $preRegistration->contact_number }}</p>
-        <p><strong>Address:</strong> {{ $preRegistration->address }}</p>
-        <p><strong>Email:</strong> {{ $preRegistration->email }}</p>
-        <p><strong>LGA Origin:</strong> {{ $preRegistration->lga_origin }}</p>
-        <p><strong>Ward:</strong> {{ $preRegistration->ward }}</p>
-        <p><strong>Highest Education:</strong> {{ $preRegistration->highest_education }}</p>
-        <p><strong>Category:</strong> {{ $preRegistration->category }}</p>
-        <p><strong>Programs:</strong> {{ $preRegistration->programs }}</p>
-        <p><strong>Status:</strong> {{ $preRegistration->status }}</p>
-    </div>    </div>
-
-    <div class="applicant-photo">
-        <img src="{{ public_path('path/to/your/applicant_photo.jpg') }}" alt="Applicant Photo">
-    </div>
-
-    <div class="footer">
-        <p>Thank you for your Pre-Registration!</p>
+    <div class="id-card">
+        <div class="header">
+            <img src="{{ public_path('/images/logo.jpeg') }}" alt="Organization Logo">
+            <h2 class="organization-name">YEEA - Jigawa State</h2>
+            <a class="website-url" href="http://www.yeea.jg.gov.ng" target="_blank">www.yeea.jg.gov.ng</a>
+        </div>
+        <div class="user-details">
+            <img @if(auth()->user()->picture == null) src="default.png" @else src="uploads/{{ auth()->user()->picture }}" @endif  alt="User Image">
+            <h3 class="user-name">{{ auth()->user()->name }}</h3>
+            <p>{{ $preRegistration->yeea_number }}</p>
+            <!-- Add more user details here -->
+        </div>
+        <div class="qr-code">
+            <img src="qrcode.png" alt="QR Code">
+        </div>
     </div>
 </body>
 </html>
