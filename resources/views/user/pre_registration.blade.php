b0f6c911f33d0e25257ddd451127fbbb11ce9e2e
katibu01 <ukmisau@gmail.com>
2023-10-18 10:12:00 +0100

    update

--- resources/views/user/pre_registration.blade.php
+++ resources/views/user/pre_registration.blade.php
@@ -1108,7 +1108,6 @@
             });
         </script>
     @endif
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.20/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.20/dist/sweetalert2.all.min.js"></script>
 
