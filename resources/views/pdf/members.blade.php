c5ab396506920b79a5b731dbdfcef303e4ab243e
katibu01 <ukmisau@gmail.com>
2023-12-02 23:09:35 +0100

    update

--- resources/views/pdf/members.blade.php
+++ resources/views/pdf/members.blade.php
@@ -13,35 +13,46 @@
     <h1 style="text-align: center; font-size: 20px; font-weight: bold;">
         Jigawa State Youth and Empowerment Agency
     </h1>
+    <h2 style="text-align: center; font-size: 15px; font-weight: bold;">{{ $collection->title }}</h2>
 
-    <h4>Members in Collection: {{ $collection->title }}</h4>
+    @php
+        $lgasInAlphabeticalOrder = $users->pluck('preRegistration.lga_origin')->unique()->sort();
+    @endphp
 
-    <table border="1" cellspacing="0" cellpadding="10" style="width: 100%; bottom: 10px;">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Full Name</th>
-                <th>Phone Number</th>
-                <th>LGA of Origin</th>
-                <th>Area of Study</th>
-                <th>YEEA Number</th>
-                <th>Ward</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $key => $user)
+    @foreach($lgasInAlphabeticalOrder as $lga)
+        @php
+            $usersInLGA = $users->where('preRegistration.lga_origin', $lga);
+        @endphp
+
+        <h4>LGA: {{ $lga }}</h4>
+
+        <table border="1" cellspacing="0" cellpadding="10" style="width: 100%; bottom: 10px;">
+            <thead>
                 <tr>
-                    <td>{{ $key+1 }}</td>
-                    <td>{{ $user->preRegistration->full_name ?? 'N/A' }}</td>
-                    <td>{{ $user->phone }}</td>
-                    <td>{{ $user->preRegistration->lga_origin ?? 'N/A' }}</td>
-                    <td>{{ $user->preRegistration->area_of_study ?? 'N/A' }}</td>
-                    <td>{{ $user->preRegistration->yeea_number ?? 'N/A' }}</td>
-                    <td>{{ $user->preRegistration->ward ?? 'N/A' }}</td>
+                    <th>#</th>
+                    <th>Full Name</th>
+                    <th>Phone Number</th>
+                    <th>LGA of Origin</th>
+                    <th>Area of Study</th>
+                    <th>YEEA Number</th>
+                    <th>Ward</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($usersInLGA as $key => $user)
+                    <tr>
+                        <td>{{ $key + 1 }}</td>
+                        <td>{{ $user->preRegistration->full_name ?? 'N/A' }}</td>
+                        <td>{{ $user->phone }}</td>
+                        <td>{{ $user->preRegistration->lga_origin ?? 'N/A' }}</td>
+                        <td>{{ $user->preRegistration->area_of_study ?? 'N/A' }}</td>
+                        <td>{{ $user->preRegistration->yeea_number ?? 'N/A' }}</td>
+                        <td>{{ $user->preRegistration->ward ?? 'N/A' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endforeach
 
     <div style="text-align: center; position: fixed; bottom: 20px; width: 100%;">
         Printed by <a href="https://yeaa.gov.ng" style="text-decoration: none;">https://yeaa.gov.ng</a> @ {{ now() }}
